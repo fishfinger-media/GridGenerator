16c9e70d4469a6e76c2f9c69dcf2a5f9d3d78c7e
Rahuli20 <111457292+Rahuli20@users.noreply.github.com>
2025-11-26 18:07:43 +0000

    templates

--- src/templates.js
+++ src/templates.js
@@ -3,109 +3,963 @@ export const templates = [
     id: 'template-1',
     name: 'Two Column',
     imageSrc: '/templates/template-1.svg',
-    rows: 1,
-    cols: 2,
-    items: [
-      { colStart: 1, colEnd: 2, rowStart: 1, rowEnd: 2 },
-      { colStart: 2, colEnd: 3, rowStart: 1, rowEnd: 2 }
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 7,
+        "rowStart": 1,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 13
+      }
     ]
   },
+
   {
     id: 'template-2',
-    name: 'Top Single, Bottom Four',
+    name: 'Two Column',
     imageSrc: '/templates/template-2.svg',
-    rows: 6,
-    cols: 12,
-    items: [
-        {
-          "colStart": 1,
-          "colEnd": 5,
-          "rowStart": 1,
-          "rowEnd": 3
-        },
-        {
-          "colStart": 1,
-          "colEnd": 5,
-          "rowStart": 5,
-          "rowEnd": 7
-        },
-        {
-          "colStart": 5,
-          "colEnd": 9,
-          "rowStart": 5,
-          "rowEnd": 7
-        },
-        {
-          "colStart": 9,
-          "colEnd": 13,
-          "rowStart": 5,
-          "rowEnd": 7
-        }
-      ]
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 1,
+        "colEnd": 13,
+        "rowStart": 7,
+        "rowEnd": 13
+      }
+    ]
   },
 
   {
     id: 'template-3',
-    name: 'Three Column',
+    name: 'Two Column',
     imageSrc: '/templates/template-3.svg',
-    rows: 6,
-    cols: 12,
-    items: [
-        {
-          "colStart": 1,
-          "colEnd": 5,
-          "rowStart": 1,
-          "rowEnd": 3
-        },
-        {
-          "colStart": 5,
-          "colEnd": 9,
-          "rowStart": 1,
-          "rowEnd": 3
-        },
-        {
-          "colStart": 9,
-          "colEnd": 13,
-          "rowStart": 1,
-          "rowEnd": 3
-        },
-        {
-          "colStart": 9,
-          "colEnd": 13,
-          "rowStart": 3,
-          "rowEnd": 5
-        },
-        {
-          "colStart": 5,
-          "colEnd": 9,
-          "rowStart": 3,
-          "rowEnd": 5
-        },
-        {
-          "colStart": 1,
-          "colEnd": 5,
-          "rowStart": 3,
-          "rowEnd": 5
-        },
-        {
-          "colStart": 1,
-          "colEnd": 5,
-          "rowStart": 5,
-          "rowEnd": 7
-        },
-        {
-          "colStart": 5,
-          "colEnd": 9,
-          "rowStart": 5,
-          "rowEnd": 7
-        },
-        {
-          "colStart": 9,
-          "colEnd": 13,
-          "rowStart": 5,
-          "rowEnd": 7
-        }
-      ]
-  }
-];
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 7,
+        "rowStart": 1,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 7,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 1,
+        "colEnd": 7,
+        "rowStart": 7,
+        "rowEnd": 13
+      }
+    ]
+  },
 
+
+  {
+    id: 'template-4',
+    name: 'Two Column',
+    imageSrc: '/templates/template-4.svg',
+  "rows": 12,
+  "cols": 12,
+  "items": [
+    {
+      "colStart": 1,
+      "colEnd": 7,
+      "rowStart": 1,
+      "rowEnd": 5
+    },
+    {
+      "colStart": 7,
+      "colEnd": 13,
+      "rowStart": 1,
+      "rowEnd": 5
+    },
+    {
+      "colStart": 7,
+      "colEnd": 13,
+      "rowStart": 5,
+      "rowEnd": 9
+    },
+    {
+      "colStart": 1,
+      "colEnd": 7,
+      "rowStart": 5,
+      "rowEnd": 9
+    },
+    {
+      "colStart": 1,
+      "colEnd": 7,
+      "rowStart": 9,
+      "rowEnd": 13
+    },
+    {
+      "colStart": 7,
+      "colEnd": 13,
+      "rowStart": 9,
+      "rowEnd": 13
+    }
+  ]
+  },
+
+  {
+    id: 'template-5',
+    name: 'Two Column',
+    imageSrc: '/templates/template-5.svg',
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 5,
+        "rowStart": 1,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 1,
+        "colEnd": 5,
+        "rowStart": 7,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 5,
+        "colEnd": 9,
+        "rowStart": 7,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 5,
+        "colEnd": 9,
+        "rowStart": 1,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 9,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 9,
+        "colEnd": 13,
+        "rowStart": 7,
+        "rowEnd": 13
+      }
+    ]
+  },  
+
+  {
+    id: 'template-6',
+    name: 'Two Column',
+    imageSrc: '/templates/template-6.svg',
+    "rows": 12,
+  "cols": 12,
+  "items": [
+    {
+      "colStart": 1,
+      "colEnd": 5,
+      "rowStart": 1,
+      "rowEnd": 5
+    },
+    {
+      "colStart": 5,
+      "colEnd": 9,
+      "rowStart": 1,
+      "rowEnd": 5
+    },
+    {
+      "colStart": 9,
+      "colEnd": 13,
+      "rowStart": 1,
+      "rowEnd": 5
+    },
+    {
+      "colStart": 9,
+      "colEnd": 13,
+      "rowStart": 5,
+      "rowEnd": 9
+    },
+    {
+      "colStart": 5,
+      "colEnd": 9,
+      "rowStart": 5,
+      "rowEnd": 9
+    },
+    {
+      "colStart": 1,
+      "colEnd": 5,
+      "rowStart": 5,
+      "rowEnd": 9
+    },
+    {
+      "colStart": 1,
+      "colEnd": 5,
+      "rowStart": 9,
+      "rowEnd": 13
+    },
+    {
+      "colStart": 5,
+      "colEnd": 9,
+      "rowStart": 9,
+      "rowEnd": 13
+    },
+    {
+      "colStart": 9,
+      "colEnd": 13,
+      "rowStart": 9,
+      "rowEnd": 13
+    }
+  ]
+  },
+
+  {
+    id: 'template-7',
+    name: 'Two Column',
+    imageSrc: '/templates/template-7.svg',
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 4,
+        "rowStart": 1,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 1,
+        "colEnd": 4,
+        "rowStart": 7,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 4,
+        "colEnd": 7,
+        "rowStart": 7,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 4,
+        "colEnd": 7,
+        "rowStart": 1,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 7,
+        "colEnd": 10,
+        "rowStart": 1,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 10,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 10,
+        "colEnd": 13,
+        "rowStart": 7,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 7,
+        "colEnd": 10,
+        "rowStart": 7,
+        "rowEnd": 13
+      }
+    ]
+  },
+
+  {
+    id: 'template-8',
+    name: 'Two Column',
+    imageSrc: '/templates/template-8.svg',
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 7,
+        "rowStart": 1,
+        "rowEnd": 4
+      },
+      {
+        "colStart": 1,
+        "colEnd": 7,
+        "rowStart": 4,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 1,
+        "colEnd": 7,
+        "rowStart": 7,
+        "rowEnd": 10
+      },
+      {
+        "colStart": 1,
+        "colEnd": 7,
+        "rowStart": 10,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 10,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 7,
+        "rowEnd": 10
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 4,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 4
+      }
+    ]
+  },
+
+  {
+    id: 'template-9',
+    name: 'Two Column',
+    imageSrc: '/templates/template-9.svg',
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 7,
+        "rowStart": 1,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 7,
+        "rowEnd": 13
+      }
+    ]
+  },
+
+  {
+    id: 'template-10',
+    name: 'Two Column',
+    imageSrc: '/templates/template-10.svg',
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 7,
+        "rowStart": 1,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 5
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 5,
+        "rowEnd": 9
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 9,
+        "rowEnd": 13
+      }
+    ]
+  },
+
+  {
+    id: 'template-11',
+    name: 'Two Column',
+    imageSrc: '/templates/template-11.svg',
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 8,
+        "rowStart": 1,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 8,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 4
+      },
+      {
+        "colStart": 8,
+        "colEnd": 13,
+        "rowStart": 4,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 8,
+        "colEnd": 13,
+        "rowStart": 7,
+        "rowEnd": 10
+      },
+      {
+        "colStart": 8,
+        "colEnd": 13,
+        "rowStart": 10,
+        "rowEnd": 13
+      }
+    ]
+  },
+
+  {
+    id: 'template-12',
+    name: 'Two Column',
+    imageSrc: '/templates/template-12.svg',
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 9,
+        "rowStart": 1,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 9,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 9,
+        "colEnd": 13,
+        "rowStart": 7,
+        "rowEnd": 10
+      },
+      {
+        "colStart": 9,
+        "colEnd": 13,
+        "rowStart": 10,
+        "rowEnd": 13
+      }
+    ]
+  },
+
+  {
+
+    id: 'template-13',
+    name: 'Two Column',
+    imageSrc: '/templates/template-13.svg',
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 9,
+        "rowStart": 1,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 9,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 9,
+        "colEnd": 11,
+        "rowStart": 7,
+        "rowEnd": 10
+      },
+      {
+        "colStart": 9,
+        "colEnd": 11,
+        "rowStart": 10,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 11,
+        "colEnd": 13,
+        "rowStart": 7,
+        "rowEnd": 10
+      },
+      {
+        "colStart": 11,
+        "colEnd": 13,
+        "rowStart": 10,
+        "rowEnd": 13
+      }
+    ]
+  },
+
+  {
+    id: 'template-14',
+    name: 'Two Column',
+    imageSrc: '/templates/template-14.svg',
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 7,
+        "rowStart": 1,
+        "rowEnd": 9
+      },
+      {
+        "colStart": 1,
+        "colEnd": 7,
+        "rowStart": 9,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 10,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 7,
+        "rowEnd": 10
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 4,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 4
+      }
+    ]
+  },
+
+  {
+    id: 'template-15',
+    name: 'Two Column',
+    imageSrc: '/templates/template-15.svg',
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 7,
+        "rowStart": 1,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 5
+      },
+      {
+        "colStart": 1,
+        "colEnd": 7,
+        "rowStart": 7,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 5,
+        "rowEnd": 9
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 9,
+        "rowEnd": 13
+      }
+    ]
+  },
+
+  {
+    id: 'template-16',
+    name: 'Two Column',
+    imageSrc: '/templates/template-16.svg',
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 5,
+        "rowStart": 1,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 5,
+        "colEnd": 7,
+        "rowStart": 9,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 7,
+        "colEnd": 9,
+        "rowStart": 9,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 9,
+        "colEnd": 11,
+        "rowStart": 9,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 11,
+        "colEnd": 13,
+        "rowStart": 9,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 5,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 9
+      }
+    ]
+  },
+
+  {
+    id: 'template-17',
+    name: 'Two Column',
+    imageSrc: '/templates/template-17.svg',
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 7,
+        "rowStart": 7,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 7,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 1,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 7
+      }
+    ]
+  },
+
+  {
+    id: 'template-18',
+    name: 'Two Column',
+    imageSrc: '/templates/template-18.svg',
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 5,
+        "rowStart": 7,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 1,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 5,
+        "colEnd": 9,
+        "rowStart": 7,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 9,
+        "colEnd": 13,
+        "rowStart": 7,
+        "rowEnd": 13
+      }
+    ]
+  },
+
+  {
+    id: 'template-19',
+    name: 'Two Column',
+    imageSrc: '/templates/template-19.svg',
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 4,
+        "rowStart": 7,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 1,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 4,
+        "colEnd": 7,
+        "rowStart": 7,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 7,
+        "colEnd": 10,
+        "rowStart": 7,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 10,
+        "colEnd": 13,
+        "rowStart": 7,
+        "rowEnd": 13
+      }
+    ]
+  },
+
+  {
+    id: 'template-20',
+    name: 'Two Column',
+    imageSrc: '/templates/template-20.svg',
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 11,
+        "colEnd": 13,
+        "rowStart": 7,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 9,
+        "colEnd": 11,
+        "rowStart": 7,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 7,
+        "colEnd": 9,
+        "rowStart": 7,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 1,
+        "colEnd": 7,
+        "rowStart": 7,
+        "rowEnd": 13
+      }
+    ]
+  }, 
+
+  {
+    id: 'template-21',
+    name: 'Two Column',
+    imageSrc: '/templates/template-21.svg',
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 7
+      },
+      {
+        "colStart": 1,
+        "colEnd": 8,
+        "rowStart": 7,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 8,
+        "colEnd": 13,
+        "rowStart": 7,
+        "rowEnd": 9
+      },
+      {
+        "colStart": 8,
+        "colEnd": 13,
+        "rowStart": 9,
+        "rowEnd": 11
+      },
+      {
+        "colStart": 8,
+        "colEnd": 13,
+        "rowStart": 11,
+        "rowEnd": 13
+      }
+    ]
+  }, 
+
+  {
+    id: 'template-22',
+    name: 'Two Column',
+    imageSrc: '/templates/template-22.svg',
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 10,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 8
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 8,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 1,
+        "colEnd": 7,
+        "rowStart": 8,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 1,
+        "colEnd": 10,
+        "rowStart": 1,
+        "rowEnd": 8
+      }
+    ]
+  },
+
+  {
+    id: 'template-23',
+    name: 'Two Column',
+    imageSrc: '/templates/template-23.svg',
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 4,
+        "rowStart": 8,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 4,
+        "colEnd": 7,
+        "rowStart": 8,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 7,
+        "colEnd": 10,
+        "rowStart": 8,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 10,
+        "colEnd": 13,
+        "rowStart": 8,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 7,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 8
+      },
+      {
+        "colStart": 1,
+        "colEnd": 7,
+        "rowStart": 1,
+        "rowEnd": 8
+      }
+    ]
+  },
+
+  {
+    id: 'template-24',
+    name: 'Two Column',
+    imageSrc: '/templates/template-24.svg',
+    "rows": 12,
+    "cols": 12,
+    "items": [
+      {
+        "colStart": 1,
+        "colEnd": 13,
+        "rowStart": 1,
+        "rowEnd": 5
+      },
+      {
+        "colStart": 6,
+        "colEnd": 13,
+        "rowStart": 5,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 1,
+        "colEnd": 6,
+        "rowStart": 9,
+        "rowEnd": 13
+      },
+      {
+        "colStart": 1,
+        "colEnd": 6,
+        "rowStart": 5,
+        "rowEnd": 9
+      }
+    ]
+  }
+
+
+]
